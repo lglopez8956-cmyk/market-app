4fc2915ac5f846b01938d0c44a5a077d784a4645
lglopez8956-cmyk <lglopez.8956@unicesmag.edu.co>
2025-09-18 11:57:44 -0500

    Task1LoginRequest

---
+++ src/signin.php
@@ -0,0 +1,31 @@
+<?php
+//get database acces
+    require('../config/database.php');
+//step1. get database connection
+$e_mail = $_POST['email'];
+$p_wd = $_POST['passwd'];
+
+//Step 3. query to validate data
+$sql_check_user = "
+select 
+ 	u.email,
+ 	u.password
+from 
+	users u 
+where	
+	u.email = '$e_mail' and 
+	u.password = '$p_wd'
+limit 1	
+
+";
+
+//step 4. execute query
+$res_check = pg_query($conn, $sql_check_user);
+
+if(pg_num_rows($res_check)> 0){
+   echo "<scrip>alert('Go to main page !!')</script>";
+}else{
+    echo "verify data";
+}
+
+  
--- src/signup.php
+++ src/signup.php
@@ -1,44 +1,57 @@
 <?php
-    //Stop1.Get database accesss
+    //get database acces
     require('../config/database.php');
+    //get form data
+    $f_name = $_POST['fname'];
+    $l_name = $_POST['lname'];
+    $m_number = $_POST['mnumber'];
+    $ide_number = $_POST['idenumber'];
+    $e_mail = $_POST['email'];
+    $p_wd = $_POST['passwd'];
+
+    $ec_pass = password_hash($p_wd, PASSWORD_BCRYPT);
     
-    //step2.Get form-data
-    $f_name = $_POST['fname']
-    $l_name = $_POST['lname']
-    $m_number = $_POST['mnumber']
-    $id_number = $_POST['idnumber']
-    $e_mail = $_POST['email']
-    $P_wd = $_POST['passwd']
+    $check_email="
+    SELECT
+       u.email
+    FROM
+       users u
+    WHERE
+       email = '$e_mail' or 
+       ide_number = '$ide_number'
+    LIMIT 1
+    ";
+
+
+$res_check =pg_query($conn, $check_email);
+    if(pg_num_rows($res_check) >0){
+        echo "<script>alert('sucess !!! go to login')</script>";+
+        header('refresh:0;url=signup.html');
+    }else{  
     
-    //step3 -> CREATE QUERY TO INSERT INTO
+    }
+    //create query to insert into
     $query = "
-           INSERT INTO USERS (
-           firstname,
-           lastname,
-           mobile_number,
-           ide_number,
-           email,
-           password
-           
-        ) VALUES (
-        '$f_name',
-        '$l_name', 
-        '$m_number', 
-        '$id_number',
-        '$e_mail',
-        '$p_wd'
-        )
-";
-        //step4 execute query
-        $res = pg_query($conn,$query);
+    INSERT INTO users (
+        firstname,
+        lastname,
+        mobile_number, 
+        ide_number,
+        email,
+        password
 
-        //step 5 validate result
-        if($res) {
-            echo"User has been created successfully !!! ";
-             }else{ 
-                echo "something wrong!"
-
-        
-
-
-        }
+    ) VALUES (
+        '$f_name', '$l_name', '$m_number', '$ide_number', '$e_mail', '$p_wd'
+    )
+    ";
+    //execute query
+    $res = pg_query($conn, $query);
+    //validate result
+    if($res){
+        //echo "Users has been created sucessfully!!!";
+        echo "<script>alert('sucess !!! go to login')</script>";
+        header('refresh:0;url=signin.html');
+    } else {
+        echo "Something wrong!";
+    }
+?>
